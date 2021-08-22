8149b3fbc7aa901c0c65543f5749c9dc37b2dba7
szczaksacup <89272690+szczaksacup@users.noreply.github.com>
2021-08-22 13:49:47 +0200

    Delete push-worker.js

--- push-worker.js
+++
@@ -1,6 +0,0 @@
-self[`appKey`] = `cbd5ff692b73bfe8f0bbce4152eba9ae`;
-self[`hostUrl`] = `https://cdn.gravitec.net/sw`;
-self.importScripts(`${self[`hostUrl`]}/worker.js`);
-// uncomment and set path to your service worker
-// if you have one with precaching functionality (has oninstall, onactivate event listeners)
-// self.importScripts('path-to-your-sw-with-precaching')
